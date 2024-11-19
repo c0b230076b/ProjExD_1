23867682d7396647329fba59b857465a2dff6e1c
Kotaro <c0b230076b@edu.teu.ac.jp>
2024-11-19 16:32:35 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -11,18 +11,33 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_flip_img = pg.transform.flip(bg_img, True, False)
-    koukaton_img = pg.image.load("fig/3.png")
-    koukaton_img = pg.transform.flip(koukaton_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+
+            
+        #背景の描画
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_flip_img, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(bg_flip_img, [-x + 4800, 0])
-        screen.blit(koukaton_img, [300, 200])
+        # screen.blit(bg_flip_img, [-x + 4800, 0]) #これは不要かも
+        screen.blit(kk_img, kk_rct)#こうかとんの描画
         pg.display.update()
         tmr += 1        
         clock.tick(300)
