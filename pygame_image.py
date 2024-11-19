797eca6cef761e822620d88838942d445398292c
Kotaro <c0b230076b@edu.teu.ac.jp>
2024-11-19 16:15:05 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,22 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_flip_img = pg.transform.flip(bg_img, True, False)
     koukaton_img = pg.image.load("fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-(tmr%800), 0])
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_flip_img, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(bg_flip_img, [-x + 4800, 0])
         screen.blit(koukaton_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(300)
 
 
 if __name__ == "__main__":
